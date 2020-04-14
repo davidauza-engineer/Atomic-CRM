b72a3fdb4144baff0918de09ccb562b5f9e279d9
davidauza.engineer <davidauza01@gmail.com>
2020-04-14 15:20:36 -0500

    Finish Home implementation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,7 @@
 class StaticPagesController < ApplicationController
   def home; end
+
+  def log_in; end
+
+  def sign_up; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get 'login', to: 'static_pages#log_in'
+  get 'signup', to: 'static_pages#sign_up'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
