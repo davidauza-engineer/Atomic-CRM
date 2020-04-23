ea05d44e9107c8b3e088a5881c09a6b872e1191c
davidauza.engineer <davidauza01@gmail.com>
2020-04-23 15:22:26 -0500

    Add tests for Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   validates :name, length: { maximum: 255 }, presence: true
-  validates :description, length: { maximum: 1530 }
-  AMOUNT_FORMAT_REGEX = /\A\d+(?:\.\d{0,2})?\z/
+  validates :description, length: { maximum: 1530 }, allow_nil: false
+  AMOUNT_FORMAT_REGEX = /\A\d+(?:\.\d{0,2})?\z/.freeze
   validates :amount, format: { with: AMOUNT_FORMAT_REGEX }
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'Test', email: 'test@user.com', password: 'secret') }
+  let(:transaction) do
+    described_class.new(author_id: user.id, name: 'Car Sale',
+                        description: 'Porsche 911 750z', amount: 150_000)
+  end
+
+  describe 'Validations' do
+    context 'with name field' do
+      it 'validates a transaction with a name' do
+        expect(transaction.valid?).to eq true
+      end
+
+      it 'rejects a transaction with a name longer than 255 characters' do
+        transaction.name = 'a' * 256
+        expect(transaction.valid?).to eq false
+      end
+
+      it 'rejects a transaction with an empty name' do
+        transaction.name = ''
+        expect(transaction.valid?).to eq false
+      end
+
+      it 'rejects a transaction with a nil name' do
+        transaction.name = nil
+        expect(transaction.valid?).to eq false
+      end
+    end
+
+    context 'with description field' do
+      it 'validates a transaction with a description' do
+        expect(transaction.valid?).to eq true
+      end
+
+      it 'validates a transaction without a description' do
+        transaction.description = ''
+        expect(transaction.valid?).to eq true
+      end
+
+      it 'rejects a transaction with a nil description' do
+        transaction.description = nil
+        expect(transaction.valid?).to eq false
+      end
+
+      it 'rejects a transaction with a description longer that 1530 characters' do
+        transaction.description = 'a' * 1531
+        expect(transaction.valid?).to eq false
+      end
+    end
+
+    context 'with amount field' do
+      it 'validates a transaction with an amount that matches the format digit.twoDecimals' do
+        expect(transaction.valid?).to eq true
+      end
+
+      it 'rejects a transaction without an amount' do
+        transaction.amount = ''
+        expect(transaction.valid?).to eq false
+      end
+
+      it 'rejects a transaction with a nil amount' do
+        transaction.amount = nil
+        expect(transaction.valid?).to eq false
+      end
+    end
+  end
 end
