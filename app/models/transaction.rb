58bb4105df9130eb8224629ca89796c15ebf1f7e
davidauza.engineer <davidauza01@gmail.com>
2020-04-23 14:39:05 -0500

    Add validations to Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,6 @@
 class Transaction < ApplicationRecord
+  validates :name, length: { maximum: 255 }, presence: true
+  validates :description, length: { maximum: 1530 }
+  AMOUNT_FORMAT_REGEX = /\A\d+(?:\.\d{0,2})?\z/
+  validates :amount, format: { with: AMOUNT_FORMAT_REGEX }
 end
