7040039389d370e012ec7887ea76488f3a5b9979
davidauza.engineer <davidauza01@gmail.com>
2020-04-27 10:43:36 -0500

    Create Categories model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_172242) do
+ActiveRecord::Schema.define(version: 2020_04_27_153755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", limit: 255, default: "My custom category"
+    t.string "icon", limit: 255, default: "my_custom_category.svg"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.integer "author_id", null: false
