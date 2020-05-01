c45d0e823d458e0842d763f6f3b5577f9b080fcb
davidauza.engineer <davidauza01@gmail.com>
2020-05-01 16:25:53 -0500

    Add tests for transactions/index.html.erb view

--- spec/views/transactions/index.html.erb_spec.rb
+++ spec/views/transactions/index.html.erb_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe 'transactions/index.html.erb', type: :view do
   before do
     user = User.create(name: 'Test', email: 'email@test.com', password: 'secret')
-    user.transactions.create(amount: 99.99)
+    @user_transaction = user.transactions.create(amount: 99.99, name: 'Test Transaction')
     @user_transactions = user.transactions
     @total_balance = user.transactions.sum(:amount)
+    @user_transactions_categories = [[]]
     render
   end
 
@@ -26,6 +27,43 @@ RSpec.describe 'transactions/index.html.erb', type: :view do
   end
 
   it "contains a span with the user's total balance" do
-    expect(rendered).to match(%r{<span>\$99.99<\/span>})
+    expect(rendered).to match(/\$99.99/)
+  end
+
+  describe 'transaction cards' do
+
+    shared_examples 'card content' do
+      it 'displays the transaction name' do
+        expect(rendered).to match(/Test Transaction/)
+      end
+
+      it 'displays the transaction created_at date in a dd Mon yy format' do
+        expect(rendered).to match(/#{@user_transaction.created_at.strftime("%d %b %Y")}/)
+      end
+
+      it "displays the transaction's amount" do
+        expect(rendered).to match(/99.99/)
+      end
+    end
+
+    context 'with uncategorized transaction' do
+
+      it_behaves_like 'card content'
+
+      it "contains an uncategorized-background css class for the icon's transaction" do
+        expect(rendered).to match(/uncategorized-background/)
+      end
+    end
+
+    context 'with categorized transaction' do
+
+      it_behaves_like 'card content'
+
+      it "contains a category-background css class for the icon's transaction" do
+        @user_transactions_categories = [[Category.new]]
+        render
+        expect(rendered).to match(/category-background/)
+      end
+    end
   end
 end
